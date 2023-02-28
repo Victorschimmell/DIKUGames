52febd8ed286e088ef7f21f5227601e1a97f5b7a
zch403 <116637957+zch403@users.noreply.github.com>
2023-02-28 19:56:47 +0100

    6.4 start
    Indsat det første af 6.4

--- Galaga/Game.cs
+++ Galaga/Game.cs
@@ -10,16 +10,29 @@ using System.Collections.Generic;
 
 namespace Galaga;
 public class Game : DIKUGame, IGameEventProcessor {
+    private EntityContainer<Enemy> enemies;
     private GameEventBus eventBus;
     private Player player;
     public Game(WindowArgs windowArgs) : base(windowArgs) {
+        // Player
         player = new Player(
             new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
             new Image(Path.Combine("Assets", "Images", "Player.png")));
+        // EventBus
         eventBus = new GameEventBus();
         eventBus.InitializeEventBus(new List<GameEventType> { GameEventType.InputEvent });
         window.SetKeyEventHandler(KeyHandler);
         eventBus.Subscribe(GameEventType.InputEvent, this);
+        // Enemies
+        List<Image> images = ImageStride.CreateStrides
+            (4, Path.Combine("Assets", "Images", "BlueMonster.png"));
+        const int numEnemies = 8;
+        enemies = new EntityContainer<Enemy>(numEnemies);
+        for (int i = 0; i < numEnemies; i++) {
+            enemies.AddEntity(new Enemy(
+                new DynamicShape(new Vec2F(0.1f + (float)i * 0.1f, 0.9f), new Vec2F(0.1f, 0.1f)),
+                new ImageStride(80, images)));
+        }
     }
     public override void Render() {
         player.Render();
