e47e3fa37d01877a3918524064985236881d6cc3
zch403 <116637957+zch403@users.noreply.github.com>
2023-03-15 10:37:19 +0100

    Update Health.cs
    Health can't be reduced to less than zero and can't increase with the method LoseHealth()

--- Galaga/GameText/Health.cs
+++ Galaga/GameText/Health.cs
@@ -4,20 +4,26 @@ using DIKUArcade.Math;
 namespace Galaga.GameText {
     public class Health: IGameText {
     private int health;
+    private int maxHealth;
     private Text display;
     public Text Display => display;
     public Health (Vec2F position, Vec2F extent) {
-        health = 3;
+        maxHealth = 3;
+        health = maxHealth;
         display = new Text (health.ToString(), position, extent);
         display.SetColor(new Vec3F(1f,1f,1f));
     }
     /// <summary>
-    /// This method reduces the health by 1 and checks whether or not the HP is 0. The method returns 
-    /// health after decreasing it. It is then handled in Game because it is not Health's responsibility
-    /// to handle what it means, it is Game's responsibility. 
+    /// This method reduces the health by an amount and checks whether or not the HP is 0. The method  
+    /// returns health after decreasing it. It is then handled in Game because it is not Health's 
+    /// responsibility to handle what it means, it is Game's responsibility. 
+    /// The hp is not reduced if the amount to lose is negative and the hp can't be reduced to less
+    /// than 0.
     /// </summary>
     public int LoseHealth (int amount) {
-        health -= amount;
+        if (amount > 0) {
+            health = System.Math.Max(0, health - amount);
+        }
         return health;
     }
     public void RenderGameText () {
