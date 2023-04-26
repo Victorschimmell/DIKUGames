52b25618b2a0124ee17a2bc31625452777bc2113
Victorschimmell <Victorschimmell@gmail.com>
2023-04-26 19:35:20 +0200

    ASCIIReader & MapLoader optimization

--- Breakout/LevelLoading/ASCIIReader.cs
+++ Breakout/LevelLoading/ASCIIReader.cs
@@ -7,94 +7,104 @@ using Breakout.Blocks;
 
 namespace Breakout.LevelLoading {
     public class ASCIIReader {
-        private readonly Dictionary<string, string> legend;
-        private readonly List<string> map;
-        private readonly Dictionary<string, string> meta;
+        private string currentFile;
+        private string[] fileLines;
+        private Dictionary<string, string> legend;
+        private List<string> map;
+        private Dictionary<string, string> meta;
         public ASCIIReader(string fileName) {
+            currentFile = fileName;
             if (File.Exists(fileName)) {
-                // Loading all lines from the file
-                string[] fileLines = File.ReadAllLines(fileName);
-                
-                // Finding the upper- and lowerbound of the map in the file
-                int currentIndex = 0;
-                int indexMinLoad = 0;
-                int indexMaxLoad = 0;
-                foreach (string line in fileLines) {
-                    if (line  == "Map:") {
-                        indexMinLoad = currentIndex;
-                    } else if (line == "Map/") {
-                        indexMaxLoad = currentIndex;
-                        break;
-                    } 
-                    currentIndex++;
-                }
-
-                map = new List<string>();
-                if (indexMaxLoad > indexMinLoad) {
-                    for (int i = indexMinLoad + 1; i < indexMaxLoad; i++) {
-                        map.Add(fileLines[i]);
-                    }
-                }
-
-
-                // META
-                currentIndex = 0;
-                indexMinLoad = 0;
-                indexMaxLoad = 0;
-                foreach (string line in fileLines) {
-                    if (line  == "Meta:") {
-                        indexMinLoad = currentIndex;
-                    } else if (line == "Meta/") {
-                        indexMaxLoad = currentIndex;
-                    } 
-                    currentIndex++;
-                }
-
-                meta = new Dictionary<string, string>();
-                if (indexMaxLoad > indexMinLoad) {
-                    for (int i = indexMinLoad + 1; i < indexMaxLoad; i++) {
-                        string[] metaLines = fileLines[i].Split(": ");
-                        if (metaLines.Length == 2){
-                            meta.Add(metaLines[0], metaLines[1]);
-                        }
-                    }
-                }
-
-                // Legend
-                currentIndex = 0;
-                indexMinLoad = 0;
-                indexMaxLoad = 0;
-                foreach (string line in fileLines) {
-                    if (line  == "Legend:") {
-                        indexMinLoad = currentIndex;
-                    } else if (line == "Legend/") {
-                        indexMaxLoad = currentIndex;
-                    } 
-                    currentIndex++;
-                }
-
-                legend = new Dictionary<string, string>();
-                if (indexMaxLoad > indexMinLoad) {
-                    for (int i = indexMinLoad + 1; i < indexMaxLoad; i++) {
-                        string[] legendLines = fileLines[i].Split(") ");
-                        if (legendLines.Length == 2){
-                            legend.Add(legendLines[0], legendLines[1]);
-                        }
-                    }
-                }
+                fileLines = File.ReadAllLines(fileName);
             }
         }
 
-        public Dictionary<string, string> GetMeta() {
-            return meta;
-        }
+        private List<string> readMap() {
+            int currentIndex = 0;
+            int indexMinRead = 0;
+            int indexMaxRead = 0;
+            foreach (string line in fileLines) {
+                if (line  == "Map:") {
+                    indexMinRead = currentIndex;
+                } else if (line == "Map/") {
+                    indexMaxRead = currentIndex;
+                    break;
+                } 
+                currentIndex++;
+            }
 
-        public List<string> GetMap() {
+            map = new List<string>();
+            if (indexMaxRead > indexMinRead) {
+                for (int i = indexMinRead + 1; i < indexMaxRead; i++) {
+                    map.Add(fileLines[i]);
+                }
+            }
+
             return map;
         }
 
-        public Dictionary<string, string> GetLegend() {
+        private Dictionary<string, string> readMeta() {
+            int currentIndex = 0;
+            int indexMinRead = 0;
+            int indexMaxRead = 0;
+            foreach (string line in fileLines) {
+                if (line  == "Meta:") {
+                    indexMinRead = currentIndex;
+                } else if (line == "Meta/") {
+                    indexMaxRead = currentIndex;
+                } 
+                currentIndex++;
+            }
+
+            meta = new Dictionary<string, string>();
+            if (indexMaxRead > indexMinRead) {
+                for (int i = indexMinRead + 1; i < indexMaxRead; i++) {
+                    string[] metaLines = fileLines[i].Split(": ");
+                    if (metaLines.Length == 2){
+                        meta.Add(metaLines[0], metaLines[1]);
+                    }
+                }
+            }
+
+            return meta;
+        }
+
+        private Dictionary<string, string> readLegend() {
+            int currentIndex = 0;
+            int indexMinRead = 0;
+            int indexMaxRead = 0;
+            foreach (string line in fileLines) {
+                if (line  == "Legend:") {
+                    indexMinRead = currentIndex;
+                } else if (line == "Legend/") {
+                    indexMaxRead = currentIndex;
+                } 
+                currentIndex++;
+            }
+
+            legend = new Dictionary<string, string>();
+            if (indexMaxRead > indexMinRead) {
+                for (int i = indexMinRead + 1; i < indexMaxRead; i++) {
+                    string[] legendLines = fileLines[i].Split(") ");
+                    if (legendLines.Length == 2){
+                        legend.Add(legendLines[0], legendLines[1]);
+                    }
+                }
+            }
+
             return legend;
+        }
+
+        public List<string> GetMap() {
+            return readMap();
+        }
+
+        public Dictionary<string, string> GetMeta() {
+            return readMeta();
+        }
+
+        public Dictionary<string, string> GetLegend() {
+            return readLegend();
         }
     }
 }
--- Breakout/LevelLoading/MapLoader.cs
+++ Breakout/LevelLoading/MapLoader.cs
@@ -5,26 +5,24 @@ using Breakout.Blocks;
 
 namespace Breakout.LevelLoading {
     public class MapLoader {
+        private ASCIIReader reader;
         public EntityContainer<Block> Blocks {get;}
-        private List<string> map;
         private Dictionary<string, string> meta;
         private Dictionary<string, string> legend;
         public MapLoader(ASCIIReader reader) {
-            map = reader.GetMap();
-            meta = reader.GetMeta();
-            legend = reader.GetLegend();
+            this.reader = reader;
             Blocks = new EntityContainer<Block>();
         }
 
         public void LoadBlocks() {
             int currentHeight = 0;
-            int mapHeight = map.Count();
-            foreach (string line in map) {
+            int mapHeight = reader.GetMap().Count();
+            foreach (string line in reader.GetMap()) {
                 currentHeight++;
                 char[] individualSymbols = line.ToCharArray();
                 float length = (float)individualSymbols.Length;
                 for (int i = 0; i < length; i++) {
-                    foreach (KeyValuePair<string, string> value in legend) {
+                    foreach (KeyValuePair<string, string> value in reader.GetLegend()) {
                         if (value.Key == individualSymbols[i].ToString()) {
                             Vec2F pos = new Vec2F((0.5f - (length / 2f * 0.08f) + (i * 0.08f)), 
                             (0.95f - 0.025f * currentHeight));
